7ca90813788c3676d3725abd477d13379fe31da4
Juan Martí <jmarti.dev@gmail.com>
2017-08-16 00:27:34 -0400

    First Commit
    Have an initial example of a progressbar.  This increase every time you press the button.

---
+++ ProgressBarExample/ProgressBarExamplePage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+
+namespace ProgressBarExample
+{
+    public partial class ProgressBarExamplePage : ContentPage
+    {
+        public ProgressBarExamplePage()
+        {
+            InitializeComponent();
+        }
+
+        void Increase_Clicked(object sender, System.EventArgs e)
+        {
+            progressbar1.Progress += .1;
+        }
+    }
+}
